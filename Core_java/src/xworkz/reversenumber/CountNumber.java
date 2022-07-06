9438c8eb55970612909a5df2f8961fcc8d72368a
Dharshanchannapatna <Dharshanv.xworkz@gmail.com>
2022-07-06 08:50:01 +0530

    countnumber

---
+++ Core_java/src/xworkz/reversenumber/CountNumber.java
@@ -0,0 +1,12 @@
+package xworkz.reversenumber;
+
+public class CountNumber {
+	
+public static void main(String[] args) {
+	int number=123456;
+	System.out.println(String.valueOf(number).length());
+}
+
+	}
+
+
